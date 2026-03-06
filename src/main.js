c2cc52f073863dc5776ca3511b79452f149f7b36
Luca Soler <e228154@edu.sbl.ch>
2026-03-06 09:29:18 +0100

    6.3 boden + decke Springen Röhren

--- src/main.js
+++ src/main.js
@@ -1,20 +1,47 @@
 import kaplay from "kaplay";
-import sc00 from "./scenes/sc-00";
-import sc01 from "./scenes/sc-01";
+
 
 const k = kaplay({
 	height: 480,
 	width: 640,
 	canvas: document.getElementById("game-canvas"),
-	background: "#82b4b4",
+	background: "#147171",
 	global: false,
 	debug: true,
 	debugKey: "r",
 });
 
-k.scene("init", sc00);
-k.scene("lvl-01", sc01);
+k.setGravity(2750);
+const player = k.add([k.circle(20),
+	k.pos(320, 460),
+	k.color(k.WHITE),
+	k.body(),
+	k.area(),]);
+const ground = k.add([k.rect(640,20),
+	k.pos (0, 460),
+	k.body({ isStatic: true }),
+	k.area(),
+	k.color(k.GREEN)]);
 
-k.go("init");
+const ceiling = k.add([k.rect(640,20),
+	k.pos (0, 0),
+	k.body({ isStatic: true }),
+	k.area(),
+	k.color(k.GREEN)]);
+
+const pipedown = k.add([k.rect(75, 175),
+	k.pos(280, 305),
+	k.color(k.RED),
+	k.area()]);
+
+	const pipeup = k.add([k.rect(75, 175),
+	k.pos(280, 0),
+	k.color(k.RED),
+	k.area()]);
+	// röhre gemeinsam höhe 350 programieren
+	// bewegung der röhren und neue erstellen
+
+	player.onKeyPress("space", () => {	player.jump();
+});
 
 export default k;
